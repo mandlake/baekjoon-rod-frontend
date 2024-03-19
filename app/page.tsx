a396030b0d0a4db37e04760732e18790ce538407
Sion Kim <94289633+mandlake@users.noreply.github.com>
2024-03-19 18:33:02 +0900

    240319 axios

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,9 @@
 "use client";
 
+import axios from "axios";
 import { useState } from "react";
+
+const SERVER = "http://localhost:8080";
 
 export default function Home() {
   const [name, setName] = useState("");
@@ -9,13 +12,27 @@ export default function Home() {
     setName(e.target.value);
   };
   const handleClick = () => {
+    const url = `${SERVER}/name`;
+    const data = { name: name };
+    const config = {
+      headers: {
+        "Cache-Control": "no-cache",
+        "Content-Type": "application/json",
+        Authorization: `Bearer blah ~`,
+        "Access-Control-Allow-Origin": "*",
+      },
+    };
+
     alert(name + "님 안녕하세요.");
+    axios.post(url, data, config).then((res) => {
+      alert(JSON.stringify(res.data) + "님의 계정이 존재합니다.");
+    });
   };
 
   return (
     <>
       <div>Welcome To React World !!!</div>
-      <input type="text" onChange={handleChange} />
+      <input type="type" onChange={handleChange} />
       <br />
       <button onClick={handleClick}>클릭</button>
     </>
