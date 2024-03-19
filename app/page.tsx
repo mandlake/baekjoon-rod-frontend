16c1e94386e62d20e61c9e94eca4310f49211a03
Sion Kim <94289633+mandlake@users.noreply.github.com>
2024-03-19 15:17:42 +0900

    240319 useState

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,23 @@
-import Image from "next/image";
+"use client";
+
+import { useState } from "react";
 
 export default function Home() {
+  const [name, setName] = useState("");
+
+  const handleChange = (e: any) => {
+    setName(e.target.value);
+  };
+  const handleClick = () => {
+    alert(name + "님 안녕하세요.");
+  };
+
   return (
     <>
-      <div>Welcome To React !!</div>
-      <h3>당신의 이름은</h3>
-      <input type="text" />
+      <div>Welcome To React World !!!</div>
+      <input type="text" onChange={handleChange} />
       <br />
-      <button>전송</button>
+      <button onClick={handleClick}>클릭</button>
     </>
   );
 }
